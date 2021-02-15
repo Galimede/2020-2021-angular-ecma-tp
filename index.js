2634845aef5c9c0fd2e15b0f56ca40e7b1396716
Galimede <mat.degand@outlook.fr>
2021-02-15 16:50:15 +0100

    :sparkles: API endpoint implementation

--- index.js
+++ index.js
@@ -4,13 +4,77 @@ import axios from 'axios';
 
 const app = fastify({ logger: true });
 
-app.get('/', async (req, res) => {
-  return {
-    message: `Welcome to Node Babel with ${
-      req.body?.testValue ?? 'no testValue'
-    }`,
-  };
+async function getFoxImage() {
+  
+  try {
+    const fox = await axios.get('https://randomfox.ca/floof/');
+    return fox.data.image;
+  } catch(e) {
+    console.error(`Error : ${e}`);
+    return null;
+  }
+
+}
+
+async function getFactCat(amount = 0) {
+  try {
+    
+    let fact;
+    
+    if(fact <= 0) {
+      fact = await axios
+      .get(`https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1`);  
+    } else {
+      fact = await axios
+      .get(`https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=${amount}`);
+      
+    }
+    
+    const filteredFacts = fact.data.map((fact) => {
+      return fact.text;
+    });
+    
+    return filteredFacts;
+  
+  } catch(e) {
+    console.error(`Error : ${e}`);
+    return null;
+  }
+  
+}
+
+async function getHolidays(countryCode = 'FR') {
+  
+  try {
+    const currentDate = new Date(Date.now());
+    const currentYear = currentDate.getFullYear();
+    const holidays = await axios.get(`https://date.nager.at/api/v2/PublicHolidays/2021/${countryCode}`);
+    return holidays.data;  
+  } catch(e) {
+    console.error(`Error : ${e}`);
+    return null;
+  }
+}
+
+async function fetchApis(countryCode) {
+ const foxImage = await getFoxImage();
+ const catFacts = await getFactCat(3);
+ const holidays = await getHolidays(countryCode);
+ 
+ return {
+   "foxPicture": foxImage,
+   "catFacts": catFacts,
+   "holidays": holidays
+ }
+
+}
+
+app.post('/', async (req, res) => {
+  
+  return await fetchApis(req.body.countryCode);
+
 });
+
 
 // Run the server!
 const start = async () => {
