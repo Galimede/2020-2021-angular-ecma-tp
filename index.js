dba030126079ccaf230255e4142803aecb040eac
Galimede <mat.degand@outlook.fr>
2021-02-15 16:52:34 +0100

    :art: Format code

--- index.js
+++ index.js
@@ -5,76 +5,69 @@ import axios from 'axios';
 const app = fastify({ logger: true });
 
 async function getFoxImage() {
-  
   try {
     const fox = await axios.get('https://randomfox.ca/floof/');
     return fox.data.image;
-  } catch(e) {
+  } catch (e) {
     console.error(`Error : ${e}`);
     return null;
   }
-
 }
 
 async function getFactCat(amount = 0) {
   try {
-    
     let fact;
-    
-    if(fact <= 0) {
-      fact = await axios
-      .get(`https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1`);  
+
+    if (fact <= 0) {
+      fact = await axios.get(
+        `https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1`,
+      );
     } else {
-      fact = await axios
-      .get(`https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=${amount}`);
-      
+      fact = await axios.get(
+        `https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=${amount}`,
+      );
     }
-    
+
     const filteredFacts = fact.data.map((fact) => {
       return fact.text;
     });
-    
+
     return filteredFacts;
-  
-  } catch(e) {
+  } catch (e) {
     console.error(`Error : ${e}`);
     return null;
   }
-  
 }
 
 async function getHolidays(countryCode = 'FR') {
-  
   try {
     const currentDate = new Date(Date.now());
     const currentYear = currentDate.getFullYear();
-    const holidays = await axios.get(`https://date.nager.at/api/v2/PublicHolidays/2021/${countryCode}`);
-    return holidays.data;  
-  } catch(e) {
+    const holidays = await axios.get(
+      `https://date.nager.at/api/v2/PublicHolidays/2021/${countryCode}`,
+    );
+    return holidays.data;
+  } catch (e) {
     console.error(`Error : ${e}`);
     return null;
   }
 }
 
 async function fetchApis(countryCode) {
- const foxImage = await getFoxImage();
- const catFacts = await getFactCat(3);
- const holidays = await getHolidays(countryCode);
- 
- return {
-   "foxPicture": foxImage,
-   "catFacts": catFacts,
-   "holidays": holidays
- }
+  const foxImage = await getFoxImage();
+  const catFacts = await getFactCat(3);
+  const holidays = await getHolidays(countryCode);
 
+  return {
+    foxPicture: foxImage,
+    catFacts: catFacts,
+    holidays: holidays,
+  };
 }
 
 app.post('/', async (req, res) => {
-  
   return await fetchApis(req.body.countryCode);
-
 });
-
 
 // Run the server!
 const start = async () => {
